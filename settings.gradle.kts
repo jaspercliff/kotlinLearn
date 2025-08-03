decb1e68d7bb5723a263d93393b73b3d52e09292
jasper <2190320413@qq.com>
2025-08-03 11:49:31 +0800

    utils

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,10 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+pluginManagement {
+    plugins {
+        id 'org.jetbrains.kotlin.jvm' version '2.2.0'
+    }
 }
-rootProject.name = "kotlinLearn"
+plugins {
+    id 'org.gradle.toolchains.foojay-resolver-convention' version '0.8.0'
+}
+rootProject.name = 'kotlinLearn'
+include 'utils'
