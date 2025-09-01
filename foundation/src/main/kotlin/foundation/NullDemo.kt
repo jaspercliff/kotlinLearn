0972bda536d17e0d783630a6b02bb4dd25ec1fb3
jasper <2190320413@qq.com>
2025-09-01 19:54:13 +0800

    kotlin null safety

--- foundation/src/main/kotlin/foundation/NullDemo.kt
+++ foundation/src/main/kotlin/foundation/NullDemo.kt
@@ -1,7 +1,14 @@
 package com.jasper.foundation
 
+import kotlin.random.Random
+
 fun main(){
-    val name:String? = null  //可空类型
+//    test = null  编译错误 string不能为null
+    var name:String? = null  //可空类型
+    val num = Random.nextInt(0,10)
+    if (num>0){
+        name = "jasper"
+    }
     println(name?.length)   // 安全调用
     print(name?.length ?: "default")
 }
